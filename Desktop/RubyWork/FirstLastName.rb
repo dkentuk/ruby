ed2a1be8969ddd7b4b878c733909bce188ae296b
David Kent <dk654@hotmail.co.uk>
2014-09-30 13:35:13 +0100

    I start of exercise commit

---
+++ Desktop/RubyWork/FirstLastName.rb
@@ -0,0 +1,7 @@
+#program that will ask you for your first and last name before welcoming the peson
+
+puts 'Please enter first name: '
+first_name = gets.chomp
+puts 'Please enter last name: '
+last_name = gets.chomp
+puts 'Welcome to ruby ' + first_name + ' ' + last_name
---
+++ Desktop/RubyWork/case.rb
@@ -0,0 +1,8 @@
+input = gets.chomp.to_i
+case input
+when 1,2,3 then puts '1, 2, or 3'
+when 10 then    puts '10'
+else
+    puts 'Some other number'
+    puts 'another number'
+end
