503017bc9b9a917f8574dfd7f6bb65204385ca32
David Kent <dk654@hotmail.co.uk>
2014-09-30 16:08:07 +0100

    Second commit

---
+++ Desktop/RubyWork/30192014/Exercise/exercise.rb
@@ -0,0 +1,64 @@
+require 'date'
+require 'pry'
+
+class Person
+    #creates setters and getters methods for
+    attr_accessor :first_name, :surname, :dob, :add_email, :add_phone, :full_name, :emails, :phone_numbers, :remove_email
+    #creates getter method for
+    
+    def initialize(fname, sname, ddob)
+        @first_name = fname
+        @surname = sname
+        @dob = ddob
+        @full_name = fname + ' ' + sname
+        @emails = Array.new
+        @phone_numbers = Array.new
+        
+    end
+    
+    # a method to take the email and add it into the email array
+    def add_email(p_email)
+        @emails.push(p_email) 
+    end
+    
+     # a method to take the phone numbers and add it into the phone numbers array
+    def add_phone(p_phone)
+        @phone_numbers.push(p_phone) 
+    end
+    
+    # method to remove the email at position
+    def remove_email(p_arr_position)
+        #@emails.pop(p_arr_position)
+        @emails.delete_at(p_arr_position)
+    end
+    
+    #method to remove phone number at position
+    def remove_phone(p_arr_position)
+        @phone_numbers.delete_at(p_arr_position)
+    end
+    
+    # returns the persons information
+    def to_s
+        "#{@full_name}, was born on #{@dob}.\n
+        Their email addresses are: #{@emails}.\n
+        Their phone numbers are #{@phone_numbers}"
+    end
+    
+    #print out the details in a much easier to read format
+    def print_details
+        #variables
+        name_length = @full_name.length
+        
+        #format
+        puts(@full_name)
+        puts ("-") * name_length
+        puts(@dob)
+        puts("\n")
+        puts("Email Addresses: ")
+        @emails.each {|x| puts x, "-" }
+        
+    end
+    
+end
+
+binding.pry
