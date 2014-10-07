6b0390a2662a53c0e5b2515b2593e522a063d1b0
David Kent <dk654@hotmail.co.uk>
2014-10-07 17:27:05 +0100

    Start the markup

--- Desktop/RubyWork/30192014/selenium_webdriver/Watir/watirTumblr.rb
+++ Desktop/RubyWork/30192014/selenium_webdriver/Watir/watirTumblr.rb
@@ -1,5 +1,6 @@
 #watir exercise tumblr
 require 'watir'
+require "rautomation"
 
 browser = Watir::Browser.new :firefox
 browser.goto "https://www.tumblr.com"
@@ -34,15 +35,20 @@ browser.button(:id, "signup_forms_submit").click
 
 #### Used for the photo post ####
 browser.element(:id, "new_post_label_photo").click
-target = browser.div :class => 'drop_target'
-file_in = browser.input :id => 'photo_file_input'
-target.click 
-latest_image = Dir["C:\Users\Academy8\Desktop\WatirImageTest"].sort { |a,b| File.mtime(a) <=> File.mtime(b) }.last
-puts latest_image
-browser.file_field(:id, 'photo_file_input').set latest_image
+#target = browser.div :class => 'drop_target'
+#file_in = browser.input :id => 'photo_file_input'
+#target.click
+
+browser.button(:id => "photo_file_input").click
+window = RAutomation::Window.new :title => windowTitle
+window.text_field(:class =>"Edit").set("C:\Users\Academy8\Desktop\WatirImageTest/CANANADADADA.jpg")
+window.button(:value => "&Open").click
+#latest_image = Dir["C:\Users\Academy8\Desktop\WatirImageTest"].sort { |a,b| File.mtime(a) <=> File.mtime(b) }.last
+#puts latest_image
+#browser.file_field(:id, 'photo_file_input').set latest_image
 #
 #browser.file_field(:id, 'photo_file_input').set('C:\Users\Academy8\Desktop\CANANADADADA.jpg')
-
+#Requires rAutomation
 
 #browser.div(:class, "drop_target").click
 
