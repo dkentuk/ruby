e25b49d25843a9dca909d1e3838a8394f3047a6a
David Kent <dk654@hotmail.co.uk>
2014-10-07 16:49:59 +0100

    I'm an idiot

--- Desktop/RubyWork/30192014/selenium_webdriver/Watir/watirTumblr.rb
+++ Desktop/RubyWork/30192014/selenium_webdriver/Watir/watirTumblr.rb
@@ -9,21 +9,41 @@ browser.link(:href, "https://www.tumblr.com/login").click
 
 browser.text_field(id: "signup_email").when_present.set "dk654@hotmail.co.uk"
 browser.text_field(id: 'signup_password').when_present.set 'tumblr@comp118'
-
 browser.button(:id, "signup_forms_submit").click
 
-browser.element(:id, "new_post_label_text").click
+##### Used for the text post ####
+#browser.element(:id, "new_post_label_text").click
+#
+##b######rowser.link(:href, "/new/text?redirect_to=%2Fdashboard").click
+##browser.frame(:id=> "post_two_ifr")when_present.text_field(:id => /TextField/).set "test data watir"
+##fr = browser.frame(:id=> "post_two_ifr")
+##fr.body(:id, "tinymce").set "Hey"
+##browser.frame(:id,'"post_two_ifr"').body(:id,'tinymce').p.send_keys [:control, 'This is test']
+##browser.frame(:index => 0).body(:id,'tinymce').p.send_keys ##[:control, 'This is test']
+########
+#browser.send_keys("test_string")
+#browser.text_field(:id, "post_one").set("what a test")
+#button = browser.button :text, "Post"
+#button.click
+#
+##test for test string within the dashboard
+#browser.div(:class, "post_wrapper").wait_until_present
+##puts foo in the console 
+#puts "foo " if browser.text.include? "test_string"
+##################################
 
-#browser.link(:href, "/new/text?redirect_to=%2Fdashboard").click
-#browser.frame(:id=> "post_two_ifr")when_present.text_field(:id => /TextField/).set "test data watir"
-#fr = browser.frame(:id=> "post_two_ifr")
-#fr.body(:id, "tinymce").set "Hey"
-#browser.frame(:id,'"post_two_ifr"').body(:id,'tinymce').p.send_keys [:control, 'This is test']
-#browser.frame(:index => 0).body(:id,'tinymce').p.send_keys ##[:control, 'This is test']
-browser.send_keys("test_string")
-browser.text_field(:id, "post_one").set("what a test")
+#### Used for the photo post ####
+browser.element(:id, "new_post_label_photo").click
+target = browser.div :class => 'drop_target'
+file_in = browser.input :id => 'photo_file_input'
+target.click 
+latest_image = Dir["C:\Users\Academy8\Desktop\WatirImageTest"].sort { |a,b| File.mtime(a) <=> File.mtime(b) }.last
+puts latest_image
+browser.file_field(:id, 'photo_file_input').set latest_image
+#
+#browser.file_field(:id, 'photo_file_input').set('C:\Users\Academy8\Desktop\CANANADADADA.jpg')
 
-browser.div(:class, "post_wrapper").wait_until_present
-puts "Woo: " if browser.text.include? "test_string"
 
-browser.quit
+#browser.div(:class, "drop_target").click
+
+#browser.quit
